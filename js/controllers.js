50694326b6d8bec0117498e32c8442734babb844
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-11-01 02:47:38 +0100

    Add actions on edit and delete buttons

--- js/controllers.js
+++ js/controllers.js
@@ -14,4 +14,12 @@ function lendCtrl($scope, $http) {
 	$http.jsonp('http://lend-it-back.herokuapp.com/services/lends/list?callback=JSON_CALLBACK').success(function(data) {
 		$scope.lendList = data;
 	});
-}
+	$scope.editLend = function($lendId) {
+		alert('edit: ' + $lendId);
+		$('#editLendModal-' + $lendId).modal();
+	};
+	$scope.deleteLend = function($lendId) {
+		alert('delete: ' + $lendId);
+		$('#deleteLendModal-' + $lendId).modal();
+	};
+};
