4750f0120b4293186a4e290c5ad6fe4128358775
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-11-01 02:16:17 +0100

    Call JSONP (on test page)

--- js/services.js
+++ js/services.js
@@ -1,13 +1,3 @@
 'use strict';
 
 /* Services */
-
-angular.module('lendServices', ['ngResource']).
-	factory('Lend', function($resource){
-		return $resource('lends/:lendId.json', {}, {
-			query: {
-				method:'GET', 
-				params:{lendId:'lends'}, 
-				isArray:true}
-		});
-	});
