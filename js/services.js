2895a13583c820a155a82adf56d06557d7452137
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2012-11-05 21:50:56 +0100

    undate LIST lend path (/)

--- js/services.js
+++ js/services.js
@@ -6,7 +6,7 @@ angular.module('LendIt', ['ngResource']).
 factory('Lend', function($resource){
 	return $resource(server + ':P/services/lends/:action',{P: port, callback:"JSON_CALLBACK"},{
 		get:{method:'JSONP', params:{action:'2'}},
-		query:{method:'JSONP', params:{action:'list'}}, //List all
+		query:{method:'JSONP'}, //List all
 		save:{method:'POST'},
 		remove:{method:'DELETE'}
 	});
